f20b244dea8829cd480da145781a955b2ddd7d99
Hrishagni De <hrishagni95@gmail.com>
2022-02-10 14:09:49 -0500

    Remove extra comments

--- task2.py
+++ task2.py
@@ -1,10 +1,4 @@
-###############
-##Design the function "calibrate" to  return 
-# (1) intrinsic_params: should be a list with four elements: [f_x, f_y, o_x, o_y], where f_x and f_y is focal length, o_x and o_y is offset;
-# (2) is_constant: should be bool data type. False if the intrinsic parameters differed from world coordinates. 
-#                                            True if the intrinsic parameters are invariable.
-#It is ok to add other functions if you need
-###############
+
 import numpy as np
 from cv2 import imread, cvtColor, COLOR_BGR2GRAY, TERM_CRITERIA_EPS, TERM_CRITERIA_MAX_ITER, \
     findChessboardCorners, cornerSubPix, drawChessboardCorners
